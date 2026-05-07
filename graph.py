ee7f10c2a3811d47ae1b7023c2fc5f8cec7b2912
isavpicinini <isavpicinini@gmail.com>
2026-05-06 22:28:08 -0300

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,5 +1,5 @@
-#choose a prime number n
-n = 19
+#chose a prime number n
+n = 5
 
 #this creates an empty list which will be filled with edge representatives
 #for every i, it decides which j is such that R^j is the same edge as R^i
@@ -11,7 +11,7 @@ for i in range(0,n):
         if (1+i*j)%n==0:
             w.append(j)
     E.append(w)
-
+    
 #redE is the reduction of E
 #if i,j are equivalent then [i,j] and [j,i] show up in E
 #the loop tests for each pair in E if there is some other vector with coordinates swapped     
@@ -21,6 +21,7 @@ for i in range(0,len(E)):
         if len(E[i])==2:
             if E[i][1]==E[j][0]:
                 redE.remove(E[i])
+
 
 #this creates an empty list which will be filled with vertex representatives
 #for every i, it decides what other vertices are equivalent to it
@@ -44,21 +45,22 @@ redV=V.copy()
 for i in range(0,len(V)):
     if len(V[i])==3:
         for j in range(0,i):
-            if V[i][1]==V[j][0]:
-                redV.remove(V[i])
-                break
-            if V[i][2]==V[j][0]:
-                redV.remove([i])
-                break
-
+             if V[i][1]==V[j][0]:
+                 redV.remove(V[i])
+                 break
+             if V[i][2]==V[j][0]:
+                 redV.remove([i])
+                 break   
+            
 #each coset representative R^k is an edge from R^k to R^(k-1)
-#this runs through the reduced representatives and creates edges [k,k-1]            
+#this runs through the reduced representatives and creates edges [k,k-1] 
+#if k corresponds to added edge, the loop creates [k, E]           
 edges=[[0,0]]
 for i in range(1,len(redE)):
     w=[]
     if redE[i][0]==redE[i][1]:
         w.append(redE[i][0])
-        w.append(redE[i][0])
+        w.append('E'+str(redE[i][0]))
     else:
         w.append(redE[i][0]-1)
         w.append(redE[i][0])
@@ -75,6 +77,7 @@ for i in range(1,len(edges)):
                 elif len(redV[k])==3:
                     if edges[i][j]==redV[k][2]:
                         corE[i][j] = redV[k][0]
+
                         
 print(corE)
                         
